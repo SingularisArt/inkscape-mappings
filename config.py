7f147776b1258843c784da526b720bbf7cc8208f
Hashem A. Damrah <dermdorm2@gmail.com>
2022-02-23 15:19:32 -0800

    fist commit

---
+++ config.py
@@ -0,0 +1,57 @@
+import os
+import subprocess
+from rofi import Rofi
+from pathlib import Path
+import importlib.util as util
+
+
+def open_editor(commands, filename):
+    """
+    example arguments:
+        commands: -u /home/user/.config/nvim/minimal-tex-init.lua
+        filename: /tmp/lua_ELyKNz.tex
+    """
+
+    with open(filename, 'w') as file:
+        file.write('$$')
+
+    subprocess.Popen([
+        'xfce4-terminal', '--disable-server', '-e',
+        'nvim {} {}'.format(filename, commands)
+    ]).wait()
+
+
+def latex_document(latex):
+    return r"""
+    \documentclass{article}
+
+    \usepackage[utf8]{inputenc}
+    \usepackage[T1]{fontenc}
+    \usepackage{textcomp}
+    \usepackage{amsmath, amssymb}
+
+    \begin{document}
+    """ + latex + r"\end{document}"
+
+
+def import_file(name, path):
+    spec = util.spec_from_file_location(name, path)
+    module = util.module_from_spec(spec)
+    spec.loader.exec_module(module)
+    return module
+
+
+HOME = Path('~').expanduser()
+rofi = Rofi()
+
+config = {
+    'rofi_theme': None,
+    'font': 'Hack Nerd Font',
+    'font_size': '12',
+    'open_editor': open_editor,
+    'latex_document': latex_document,
+    'home': HOME,
+    'rofi': rofi,
+}
+
+# TODO: Add support for config file at ~/.config/inkscape-mappings/config.py
---
+++ editor.py
@@ -0,0 +1,42 @@
+import os
+import tempfile
+import threading
+import subprocess
+# from Xlib import X
+from config import config
+from clipboard import copy
+from constants import TARGET
+
+
+running = threading.Event()
+
+
+# def open_editor(self, commands, compile_latex):
+def open_editor(commands, compile_latex):
+    temp_file = tempfile.NamedTemporaryFile(mode='w+t', delete=False,
+                                            suffix='.tex')
+
+    config['open_editor'](commands, temp_file.name)
+
+    latex = ""
+
+    with open(temp_file.name, 'r') as reading_temp_file:
+        latex = reading_temp_file.read().strip()
+
+    os.remove(temp_file.name)
+
+    if latex != '$$':
+        if not compile_latex:
+            svg = f"""<?xml version="1.0" encoding="UTF-8" standalone="no"?>
+            <svg>
+              <text
+                 style="font-size:{config['font_size']}px; font-family:'{config['font']}';-inkscape-font-specification:'{config['font']}, Normal';fill:#000000;fill-opacity:1;stroke:none;"
+                 xml:space="preserve"><tspan sodipodi:role="line" >{latex}</tspan></text>
+            </svg>"""
+            copy(svg, target=TARGET)
+    elif latex == '$$' or not latex:
+        config['rofi'].error('No latex code found')
+
+
+open_editor('-u {}/.config/nvim/minimal-tex-init.lua'.format(config['home']),
+            True)
